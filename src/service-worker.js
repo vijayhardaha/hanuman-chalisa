2a5c14ade24f29351aa64c4455ac03a2f3318c76
vijayhardaha <vijayhardaha@gmail.com>
2023-09-25 03:58:37 +0530

    feat: add fetch event

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,3 +11,34 @@ async function activate() {
 	await Promise.all(keys.map((key) => key !== version && caches.delete(key)));
 }
 addEventListener("activate", (e) => e.waitUntil(activate()));
+
+async function handleFetch(event) {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			if (response) {
+				return response;
+			}
+
+			const fetchRequest = event.request.clone();
+
+			return fetch(fetchRequest).then((response) => {
+				if (
+					!response ||
+					response.status !== 200 ||
+					response.type !== "basic"
+				) {
+					return response;
+				}
+
+				const responseToCache = response.clone();
+
+				caches.open(version).then((cache) => {
+					cache.put(event.request, responseToCache);
+				});
+
+				return response;
+			});
+		})
+	);
+}
+addEventListener("fetch", (event) => event.respondWith(handleFetch(event)));
