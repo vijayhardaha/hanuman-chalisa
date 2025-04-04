6b7c0501effa12684fb91694b936c1de6b769eba
Vijay Hardaha <vijayhardaha@gmail.com>
2025-04-04 20:29:13 +0530

    refactor(couplets): replace coupletsData.js with JSON data import and clean up empty paragraphs

--- src/js/couplets.js
+++ src/js/couplets.js
@@ -1,4 +1,4 @@
-import { data } from "./coupletsData.js";
+import data from "../data/data.json";
 
 const copyIcon =
   '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M20 2H10c-1.103 0-2 .897-2 2v4H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2v-4h4c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zM4 20V10h10l.002 10H4zm16-6h-4v-4c0-1.103-.897-2-2-2h-4V4h10v10z"></path></svg>';
@@ -195,6 +195,7 @@ class CoupletsRenderer {
       `<h4 class="modal-doha">${title}</h4>` +
       content
         .split("\n")
+        .filter((paragraph) => paragraph.trim() !== "")
         .map((paragraph) => `<p>${formatText(paragraph)}</p>`)
         .join("");
     modalContent.appendChild(modalBody);
