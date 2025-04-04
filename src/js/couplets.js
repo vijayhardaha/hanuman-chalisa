5631222d2e92b9b2868c0ad361c6957f6847d78b
Vijay Hardaha <vijayhardaha@gmail.com>
2025-04-05 01:35:02 +0530

    style(modal): enhance modal dimensions and padding for improved layout; add close icon to modal header

--- src/js/couplets.js
+++ src/js/couplets.js
@@ -4,6 +4,8 @@ const copyIcon =
   '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M20 2H10c-1.103 0-2 .897-2 2v4H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2v-4h4c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zM4 20V10h10l.002 10H4zm16-6h-4v-4c0-1.103-.897-2-2-2h-4V4h10v10z"></path></svg>';
 const checkIcon =
   '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></path></svg>';
+const closeIcon =
+  '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>';
 
 /**
  * Shorthand for creating an HTML element.
@@ -20,7 +22,7 @@ const createElement = (tag) => document.createElement(tag);
  * @returns {string} - The formatted text with strong tags.
  */
 const formatText = (text) =>
-  text.replace(/(‘[^’]*’|“[^”]*”)/g, (match) => `<strong class="font-semibold">${match}</strong>`);
+  text.replace(/(‘[^’]*’|“[^”]*”)/g, (match) => `<span class="font-semibold">${match}</span>`);
 
 /**
  * Class responsible for rendering couplets on the page.
@@ -181,7 +183,7 @@ class CoupletsRenderer {
     modalHeader.className = "modal-header";
     modalHeader.innerHTML = `
       <h3 class="modal-title">आध्यात्मिक अर्थ</h3>
-      <span class="modal-close">&times;</span>
+      <span class="modal-close">${closeIcon}</span>
     `;
     modalHeader.querySelector(".modal-close").addEventListener("click", () => {
       document.body.removeChild(modal);
